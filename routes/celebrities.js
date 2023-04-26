4c7d9ca58556015dd151a1366b6876b23ff8953f
Robert De La Cruz <rdlc976@gmail.com>
2023-04-26 16:46:40 -0400

    * lab finished

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,38 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Celebrity = require('../models/Celebrity.model')
+const Celebrity = require("../models/Celebrity.model");
 
-router.get('/all-celebrities', (req, res, next) => {
-
+router.get("/all-celebrities", (req, res, next) => {
     Celebrity.find()
         .then((celebrities) => {
-            res.render('celebrities/celebrities.hbs', { celebrities })
+            res.render("celebrities/celebrities.hbs", { celebrities });
         })
         .catch((err) => {
-            console.log(err)
-        })
-})
+            console.log(err);
+        });
+});
 
-router.get('/add-celebrity', (req, res, next) => {
-    res.render('celebrities/new-celebrity.hbs')
-})
+router.get("/add-celebrity", (req, res, next) => {
+    res.render("celebrities/new-celebrity.hbs");
+});
 
-router.post('/add-celebrity', (req, res, next) => {
+router.post("/add-celebrity", (req, res, next) => {
+    let newCelebrity = req.body;
 
-    let newCelebrity = req.body
-
-    Celebrity.create(
-        newCelebrity
-    )
+    Celebrity.create(newCelebrity)
         .then((createdCelebrity) => {
-            console.log("Created Celebrity:", createdCelebrity)
-            res.redirect('/celebrities/all-celebrities')
+            console.log("Created Celebrity:", createdCelebrity);
+            res.redirect("/celebrities/all-celebrities");
         })
         .catch((err) => {
-            console.log(err)
-        })
+            console.log(err);
+        });
+});
 
-})
+router.get("/delete/:id", (req, res) => {
+    const { id } = req.params;
+    Celebrity.findByIdAndDelete(id)
+        .then(() => {
+            res.redirect("/celebrities/all-celebrities");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 module.exports = router;
